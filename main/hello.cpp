ba4b39932636cb473cfa548178ec39740df9b007
chandanadongre <113676441+chandanadongre@users.noreply.github.com>
2024-03-13 18:34:34 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I am Chandana Dongre, PES2UG21CS134 currently 6th sem.\n";
+  cout << "Hello, I am Chandana Dongre, PES2UG21CS134 currently 6th sem.\n";
   return 0;
 }
